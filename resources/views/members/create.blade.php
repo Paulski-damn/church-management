bafa0adb5204aaf677a3238dea63a777222fc0c8
Paulski-damn <ramospauul@gmail.com>
2025-10-13 20:46:24 +0800

    edit create blade of members

--- resources/views/members/create.blade.php
+++ resources/views/members/create.blade.php
@@ -134,7 +134,7 @@
                             </a>
                             <button type="submit"
                                 class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
-                                Add Member
+                                Add Members
                             </button>
                         </div>
                     </form>
